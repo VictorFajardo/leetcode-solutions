8160ce4e8379e83c6495a72ae0a4c05f101574e8
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-16 13:51:08 -0400

    Time: 107 ms (60.32%), Space: 45.5 MB (39.55%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.js
+++ 784-letter-case-permutation/784-letter-case-permutation.js
@@ -3,30 +3,85 @@
  * @return {string[]}
  */
 var letterCasePermutation = function(s) {
+    // Breadth-First Search
     const n = s.length
-    const response = []
+    const queue = [s] //replace with a Linked List
+    let index = 0
     
-    const permutate = function(pos, str = '') {
-        //base case
-        if (str.length === n) {
-            response.push(str)
-            return
+    while (queue.length && index < n) {
+        if (s[index] >= '0' && s[index] <= '9') {
+            index++
+        } else {
+            let qLength = queue.length        
+            for (let i = 0; i < qLength; i++) {
+                let curr = queue.shift()
+                //LowerCase
+                queue.push(curr.substr(0, index) + curr.charAt(index).toLowerCase() + curr.substr(index + 1))
+                //UpperCase
+                queue.push(curr.substr(0, index) + curr.charAt(index).toUpperCase() + curr.substr(index + 1))
+            }
+            index++
         }
-        const char = s[pos]
-        //loop
-        if (char >= 0 && char <= 9) permutate(pos + 1, str + char)
-        else {
-            permutate(pos + 1, str + char.toLowerCase())
-            permutate(pos + 1, str + char.toUpperCase())    
-        }
-        
-        
     }
     
-    permutate(0)
+    return queue
     
-    return response
+    // Depth-First Seach
+    /*const res = []
+    
+    const BFS = (s, index, res) => {
+        if (index === s.length) {
+            res.push(s)
+            return
+        }
+        
+        if (s[index] >= '0' && s[index] <= '9') {
+            BFS(s, index + 1, res)
+        } else {
+            //LowerCase
+            s = s.substr(0, index) + s.charAt(index).toLowerCase() + s.substr(index + 1)
+            BFS(s, index + 1, res)
+            //UpperCase
+            s = s.substr(0, index) + s.charAt(index).toUpperCase() + s.substr(index + 1)
+            BFS(s, index + 1, res)
+        }
+        
+        return res        
+    }
+    
+    return backtracking(s, 0, res)*/
+    
+    // Backtracking
+    /*    const res = []
+    
+    const backtracking = (s, index, res) => {
+        if (index === s.length) {
+            res.push(s)
+            return
+        }
+        
+        if (s[index] >= '0' && s[index] <= '9') {
+            backtracking(s, index + 1, res)
+        } else {
+            //LowerCase
+            s = s.substr(0, index) + s.charAt(index).toLowerCase() + s.substr(index + 1)
+            backtracking(s, index + 1, res)
+            //UpperCase
+            s = s.substr(0, index) + s.charAt(index).toUpperCase() + s.substr(index + 1)
+            backtracking(s, index + 1, res)
+        }
+        
+        return res        
+    }
+    
+    return backtracking(s, 0, res)*/
     
 };
 
-//Backtracking
+// Depth-First Seach
+
+// Breadth-First Search
+
+// Backtracking
+// time complexity: O(n) // permutations
+// space complexity: O(n) // permitations
