da66aff5ab4e08558449552b2c469fcdd79d44a2
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-17 02:00:44 -0400

    Time: 89 ms (45.62%), Space: 42 MB (81.27%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -4,10 +4,10 @@
  */
 var canMakeArithmeticProgression = function(arr) {
     arr.sort((a, b) => a - b)
-    let diff = Math.abs(arr[1] - arr[0])
+    let diff = arr[1] - arr[0]
     
     for (let i = 1; i < arr.length - 1; i++) {
-        if (Math.abs(arr[i + 1] - arr[i]) !== diff) return false
+        if (arr[i + 1] - arr[i] !== diff) return false
     }
     
     return true
