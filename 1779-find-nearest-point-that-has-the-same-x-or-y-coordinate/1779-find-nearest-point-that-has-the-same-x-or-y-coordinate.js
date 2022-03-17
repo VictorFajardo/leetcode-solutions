712e609e744f63b452569cc373e2122896f152d1
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-17 01:29:35 -0400

    Time: 92 ms (92.82%), Space: 50.7 MB (47.99%) - LeetHub

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @param {number[][]} points
+ * @return {number}
+ */
+var nearestValidPoint = function(x, y, points) {
+    let minIndex = -1
+    let minDistance = Infinity
+    
+    for (let i = 0; i < points.length; i++) {
+        let point = points[i]
+        if (!isValidPoint(x, y, point)) continue
+        let manhathanDistance = getManhattanDistance(x, y, point)
+        if (manhathanDistance < minDistance) {
+            minDistance = manhathanDistance
+            minIndex = i
+        }
+    }
+    
+    return minIndex
+    
+};
+
+var isValidPoint = function(x, y, point) {
+    return x === point[0] || y === point[1]
+}
+
+var getManhattanDistance = function (x, y, point) {
+    return Math.abs(x - point[0]) + Math.abs(y - point[1])
+}
