e6f7cff71630428ab972fc45c3643ccd15ce1116
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-17 05:23:06 -0400

    Time: 98 ms (67.05%), Space: 46.1 MB (28.49%) - LeetHub

---
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root, res = []) {
+    if (!root) return []
+    
+    res.push(root.val)
+    for (let child of root.children) {
+        preorder(child, res)
+    }
+    
+    return res
+    
+};
