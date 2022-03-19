7d0283e833dde9cbfcf92e1d8257897f3c6eff68
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-18 22:34:51 -0400

    Time: 72 ms (73.39%), Space: 42.6 MB (17.16%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -4,39 +4,33 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    const n = nums.length - 1
     let start = 0
-    let end = n
+    let end = nums.length - 1
     
-    while (start < end) {
-        let mid = start + Math.floor((end - start) / 2)
-        
-        if (nums[mid] > nums[end]) {
-            start = mid + 1
-        } else if (nums[mid] < nums[start]) {
-            end = mid
-        } else break
+    while (start <= end) {
+        let mid = Math.floor(start + (end - start) / 2)
+        if (nums[mid] === target) {
+            return mid
+        }
+        if (nums[start] <= nums[mid]) {
+            if (target >= nums[start] && target < nums[mid]) {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        } else {
+            if (target > nums[mid] && target <= nums[end]) {
+                start = mid + 1
+            } else {
+                end = mid - 1
+            }
+        }
     }
     
-    let rotatedIndex = start
-    
-    if (target >= nums[0] && target <= nums[rotatedIndex - 1]) return binarySearch(nums, 0, rotatedIndex - 1, target)
-    else if (target >= nums[rotatedIndex] && target <= nums[n]) return binarySearch(nums, rotatedIndex, n, target)
-    else return -1
+    return -1
     
 };
-    
-    var binarySearch = function(arr, start, end, target) {
 
-        while (start <= end) {
-            let mid = start + Math.floor((end - start) / 2)
-
-            if (arr[mid] > target) {
-                end = mid- 1
-            } else if (arr[mid] < target) {
-                start = mid + 1
-            } else return mid
-        }
-        
-        return -1    
-    }
+// Bynary Search 
+// time complexity: O(log(n))
+// space complexity: O(1)
