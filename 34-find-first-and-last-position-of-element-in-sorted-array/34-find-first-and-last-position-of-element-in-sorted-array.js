fbedb5e732fa724d920dbbbf88fba81fe8edf6ec
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 12:41:05 -0400

    Time: 64 ms (88.68%), Space: 42.5 MB (65.36%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,25 +4,25 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-    
-    return [findBoundElement(nums, target, true), findBoundElement(nums, target, false)]
-};
+    const findBoundElement = function(isFirst) {
+        let start = 0
+        let end = nums.length - 1
+        let bound = -1
 
-var findBoundElement = function(nums, target, isFirst) {
-    let start = 0
-    let end = nums.length - 1
-    let bound = -1
-    
-    while (start <= end) {
-        let mid = Math.floor((end + start) / 2)
-        let num = nums[mid]
-        if (num === target) {
-            if (isFirst) end =  mid - 1
+        while (start <= end) {
+            let mid = Math.floor((end + start) / 2)
+            let num = nums[mid]
+            if (num === target) {
+                if (isFirst) end =  mid - 1
+                else start = mid + 1
+                bound = mid
+            } else if (num > target) end = mid - 1
             else start = mid + 1
-            bound = mid
-        } else if (num > target) end = mid - 1
-        else start = mid + 1
+        }
+
+        return bound
     }
     
-    return bound
-}
+    return [findBoundElement(true), findBoundElement(false)]
+};
+
