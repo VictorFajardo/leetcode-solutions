3941d29c71cac364495382a099a3d11357913882
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 13:25:24 -0400

    Time: 124 ms (49.91%), Space: 49.1 MB (62.66%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -4,31 +4,25 @@
  *     this.val = (val===undefined ? 0 : val)
  *     this.left = (left===undefined ? null : left)
  *     this.right = (right===undefined ? null : right)
- * } 
+ * }
  */
-const sameTrees = (node1, node2) => {
-  if (!node1 && !node2) return true;
-  else if (!node1 || !node2) return false;
-  else if (node1.val !== node2.val) return false;
-  
-  const isLeftTreeEqual = sameTrees(node1.left, node2.left);
-  const isRightTreeEqual = sameTrees(node1.right, node2.right);
-  
-  return isLeftTreeEqual && isRightTreeEqual;
-};
-
 /**
  * @param {TreeNode} root
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
 var isSubtree = function(root, subRoot) {
-  if (!subRoot) return true; // null is a subtree of any tree
-  else if (!root) return false; // subRoot is not null, so it cant be a subtree of null
-  else if (sameTrees(root, subRoot)) return true;
-  
-  const isSubtreeOfLeft = isSubtree(root.left, subRoot);
-  const isSubtreeOfRight = isSubtree(root.right, subRoot);
-  
-  return isSubtreeOfLeft || isSubtreeOfRight;
+    if (!root) return false
+    
+    if (compare(root, subRoot)) return true
+    
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    
 };
+
+var compare = function(root, subRoot) {
+    if (!root && !subRoot) return true
+    if (!root || !subRoot || root.val !== subRoot.val) return false
+    
+    return compare(root.left, subRoot.left) && compare(root.right, subRoot.right)
+}
