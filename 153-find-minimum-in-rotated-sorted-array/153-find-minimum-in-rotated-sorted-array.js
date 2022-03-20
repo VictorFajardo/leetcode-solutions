86c61be0452f098563ee4d1383bdcf63a9e22bc4
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 14:19:31 -0400

    Time: 68 ms (78.56%), Space: 42.1 MB (58.03%) - LeetHub

---
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    const n = nums.length - 1
+    let left = 0
+    let right = n
+
+    if (nums[right] >= nums[left]) return nums[0]
+    
+    while (left <= right) {
+        let mid = left + Math.floor((right - left) / 2)
+        let midNumber = nums[mid]
+        let prevNumber = nums[mid - 1]
+        let nextNumber = nums[mid + 1]
+        if (midNumber < prevNumber) return midNumber
+        else if (midNumber > nextNumber) return nextNumber
+        else if (midNumber > nums[left]) left = mid + 1
+        else right = mid
+    }
+};
