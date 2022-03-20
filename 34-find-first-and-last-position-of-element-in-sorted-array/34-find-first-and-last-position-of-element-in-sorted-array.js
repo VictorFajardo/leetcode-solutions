5a9d9d7bff401b567af89f43eb06d574d34096ec
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 12:40:12 -0400

    Time: 74 ms (68.39%), Space: 43 MB (20.63%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -26,39 +26,3 @@ var findBoundElement = function(nums, target, isFirst) {
     
     return bound
 }
-
-// var findFirstElement = function(nums, target) {
-//     let start = 0
-//     let end = nums.length - 1
-//     let bound = -1
-    
-//     while (start <= end) {
-//         let mid = Math.floor((end + start) / 2)
-//         let num = nums[mid]
-//         if (num === target) {
-//             end = mid - 1
-//             bound = mid
-//         } else if (num > target) end = mid - 1
-//         else start = mid + 1
-//     }
-    
-//     return bound
-// }
-
-// var findLastElement = function(nums, target) {
-//     let start = 0
-//     let end = nums.length - 1
-//     let bound = -1
-    
-//     while (start < end) {
-//         let mid = Math.floor((end + start) / 2)
-//         let num = nums[mid]
-//         if (num === target) {
-//             start = mid + 1
-//             bound = mid
-//         } else if (num > target) end = mid - 1
-//         else start = mid + 1
-//     }
-    
-//     return bound
-// }
