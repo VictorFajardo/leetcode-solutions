f488e226fd677907221957e5401b1df4206ba734
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 07:51:36 -0400

    Time: 157 ms (27.32%), Space: 45.2 MB (55.59%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    const rows = grid.length
+    const cols = grid[0].length
+    
+    let total = 0
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === '1') {
+                fill(grid, i, j, rows, cols)
+                total++
+            }
+        }
+    }
+    return total
+};
+
+var fill = function(arr, i, j, rows, cols) {
+    arr[i][j] = '0'
+    
+    if (i - 1 >= 0 && arr[i - 1][j] === '1') fill(arr, i - 1, j, rows, cols)
+    if (j + 1 < cols && arr[i][j + 1] === '1') fill(arr, i, j + 1, rows, cols)
+    if (i + 1 < rows && arr[i + 1][j] === '1') fill(arr, i + 1, j, rows, cols)
+    if (j - 1 >= 0 && arr[i][j - 1] === '1') fill(arr, i, j - 1, rows, cols)
+}
