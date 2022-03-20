638366fbfa8e4ac43a9541b29b9b906189d7c434
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 13:19:07 -0400

    Time: 668 ms (5.03%), Space: 50.7 MB (5.03%) - LeetHub

---
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+    if (!root) return false
+    
+    return compare(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot)
+    
+};
+
+var compare = function(root, subRoot) {
+    if (!root && !subRoot) return true
+    else if (!root || !subRoot) return false
+    
+    console.log(root.val, subRoot.val)    
+    
+    if (root.val !== subRoot.val) return false
+    
+    return compare(root.left, subRoot.left) && compare(root.right, subRoot.right)
+}
