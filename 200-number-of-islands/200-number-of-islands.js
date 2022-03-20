34396f4284d9782b421b5411602c3ca3b2e89694
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 08:01:56 -0400

    Time: 121 ms (54.04%), Space: 44.7 MB (84.42%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -8,10 +8,19 @@ var numIslands = function(grid) {
     
     let total = 0
     
+    var fill = function(i, j) {
+        grid[i][j] = '0'
+
+        if (i - 1 >= 0 && grid[i - 1][j] === '1') fill(i - 1, j)
+        if (j + 1 < cols && grid[i][j + 1] === '1') fill(i, j + 1)
+        if (i + 1 < rows && grid[i + 1][j] === '1') fill(i + 1, j)
+        if (j - 1 >= 0 && grid[i][j - 1] === '1') fill(i, j - 1)
+    }
+    
     grid.forEach((row, i) => {
         row.forEach((col, j) => {
             if (grid[i][j] === '1') {
-                fill(grid, i, j, rows, cols)
+                fill(i, j)
                 total++
             }
         })
@@ -20,11 +29,3 @@ var numIslands = function(grid) {
     return total
 };
 
-var fill = function(arr, i, j, rows, cols) {
-    arr[i][j] = '0'
-    
-    if (i - 1 >= 0 && arr[i - 1][j] === '1') fill(arr, i - 1, j, rows, cols)
-    if (j + 1 < cols && arr[i][j + 1] === '1') fill(arr, i, j + 1, rows, cols)
-    if (i + 1 < rows && arr[i + 1][j] === '1') fill(arr, i + 1, j, rows, cols)
-    if (j - 1 >= 0 && arr[i][j - 1] === '1') fill(arr, i, j - 1, rows, cols)
-}
