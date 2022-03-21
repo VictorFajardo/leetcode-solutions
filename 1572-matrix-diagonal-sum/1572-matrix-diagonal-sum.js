21e20ac2a828b4e4366d98bfcc33dde2f0435b9b
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-20 22:16:32 -0400

    Time: 81 ms (59.36%), Space: 43.7 MB (58.72%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var diagonalSum = function(mat) {
-    const n = mat.length - 1
+    const n = mat.length
     let sum = 0
     
-    for (let i = 0; i <= n; i++) {
-        if (i === n - i) sum += mat[i][i]
-        else sum += mat[i][i] + mat[i][n - i]
+    for (let i = 0; i < n; i++) {
+        sum += mat[i][i]
+        sum += mat[i][n - 1 - i]
     }
+    
+    if (n % 2 !== 0) sum -= mat[(n - 1)/2][(n - 1)/2]
     
     return sum
 };
