68c4729a9e137e3bd2afc9bf3d73477339b2484d
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-21 00:42:43 -0400

    Time: 77 ms (75.72%), Space: 44.3 MB (41.98%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    const dict = {}
+    
+    for (let char of s) {
+        if (!(char in dict)) dict[char] = 0
+        dict[char]++
+    }
+    
+    for (let char of t) {
+        if (!(char in dict) || dict[char] === 0) return char
+        dict[char]--
+    }
+    
+};
