7bfa11de7ad528df846eb2d92d4612dda1fe10b7
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-22 01:50:45 -0400

    Time: 101 ms (25.95%), Space: 42.2 MB (57.63%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.js
@@ -7,9 +7,8 @@ var freqAlphabets = function(s) {
     let response = ''
     
     for (let i = 0; i < s.length; i++) {
-        if (i + 2 < s.length && s[i + 2] === '#') {
+        if (s[i + 2] === '#') {
             //j to z
-            // console.log(s[i] * 10 + s[i + 1] * 1 + factor)
             response += String.fromCharCode(Number(s[i] + s[i + 1]) + factor)
             i += 2
         } else {
