65a32d019505ea679884e25514d0a522fad68ca0
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-25 19:52:51 -0400

    Time: 163 ms (65.87%), Space: 49.1 MB (89.42%) - LeetHub

---
+++ 1603-design-parking-system/1603-design-parking-system.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.parkingLot = [big, medium, small]
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    if (this.parkingLot[carType - 1] !== 0) {
+        this.parkingLot[carType - 1]--
+        return true
+    } else return false
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
