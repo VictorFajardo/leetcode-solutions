5c0aba4f8cb768cbf0a844650392370c3ae52bcf
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-25 19:40:23 -0400

    Time: 85 ms (84.48%), Space: 42.6 MB (86.73%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -6,21 +6,23 @@
 var isAnagram = function(s, t) {
     if (s.length !== t.length) return false
     
-    const dictionary = {}
+    const dictionary = new Array(26).fill(0)
     
     for (let i = 0; i < s.length; i++) {
-        let char = s.charAt(i)
-        if (!(char in dictionary)) dictionary[char] = 0
+        let char = s.charCodeAt(i) - 97
         dictionary[char]++
     }
     
     for (let i = 0; i < t.length; i++) {
-        let char = t.charAt(i)
-        if (!(char in dictionary)) return false
+        let char = t.charCodeAt(i) - 97
         dictionary[char]--
-        if (dictionary[char] === 0) delete dictionary[char]
+        if (dictionary[char] < 0) return false
     }
     
     return true
     
 };
+
+// Array
+// time complexity: O(n || m)
+// space complexity: O(n || m)
