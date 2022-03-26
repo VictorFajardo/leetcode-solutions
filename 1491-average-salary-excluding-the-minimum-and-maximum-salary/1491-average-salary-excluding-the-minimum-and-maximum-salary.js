0ed11d78162beb12e37a4cb242ffb3a2843fd9b3
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-25 21:06:01 -0400

    Time: 52 ms (99.07%), Space: 42.4 MB (18.27%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    let minSalary = Infinity
+    let maxSalary = -Infinity
+    let sum = 0
+    
+    for (let employee of salary) {
+        minSalary = Math.min(minSalary, employee)
+        maxSalary = Math.max(maxSalary, employee)
+        sum += employee
+    }
+    
+    return (sum - minSalary - maxSalary) / (salary.length - 2)
+    
+};
