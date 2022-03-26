ae1a75ef9d7398ad3817d9001837a52b7c5cf4eb
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-25 22:43:04 -0400

    Time: 102 ms (51.27%), Space: 44.7 MB (66.40%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.js
+++ 547-number-of-provinces/547-number-of-provinces.js
@@ -5,6 +5,42 @@
 var findCircleNum = function(isConnected) {
     const n = isConnected.length
     const visited = new Array(n).fill(false)
+    let count = 0
+    
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(isConnected, visited, i, n)
+            count++
+        }
+    }
+    
+    return count
+    
+};
+
+var dfs = function(matrix, visited, index, total) {
+    visited[index] = true
+    
+    for (let i = 0; i < total; i++) {
+        if (matrix[index][i] === 1 && !visited[i]) {
+            dfs(matrix, visited, i, total)
+        }
+    }
+}
+
+// Depth First Search
+// time complexity: O(n^2)
+// space complexity: O(n)
+
+
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+/*
+var findCircleNum = function(isConnected) {
+    const n = isConnected.length
+    const visited = new Array(n).fill(false)
     const queue = new Queue()
     let count = 0
     
@@ -27,7 +63,7 @@ var findCircleNum = function(isConnected) {
     return count
     
 };
-
+*/
 // Breadth First Search
 // time complexity: O(n^2)
 // space complexity: O(n)
