7cf11be165581ed2b2936699e11b667d889fe11c
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-28 05:39:28 -0400

    Time: 96 ms (91.07%), Space: 45.5 MB (49.76%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -6,13 +6,10 @@ var largestPerimeter = function(nums) {
     nums.sort((a, b) => b - a)
     
     for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] >= nums[i + 1] + nums[i + 2]) continue
-        return nums[i] + nums[i + 1] + nums[i + 2]
+        if (nums[i] < nums[i + 1] + nums[i + 2])
+            return nums[i] + nums[i + 1] + nums[i + 2]
     }
     
     return 0
+    
 };
-
-// Sorting + Array
-// time complexity: O(n*log(n))
-// space complexity: O(1)
