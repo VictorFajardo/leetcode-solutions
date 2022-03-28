09e0bc53e1824422d265be0d383a00d6199cd164
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-28 06:36:21 -0400

    Time: 88 ms (85.10%), Space: 44.8 MB (80.40%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permuteUnique = function(nums) {
+    nums.sort((a, b) => a - b)
+    const n = nums.length
+    const response = []
+    const visited = new Array(n).fill(false)
+    const permutation = []
+    
+    const backtracking = function(permutation = [], visited = []) {
+        //base case
+        if (permutation.length === n) {
+            response.push(permutation.slice(0))    
+        } 
+        
+        for (let i = 0; i < n; i++) {
+            if (visited[i]) continue
+            if (i > 0 && nums[i] === nums[i - 1] && visited[i - 1]) continue
+            //add
+            visited[i] = true
+            permutation.push(nums[i])
+            //backtrack
+            backtracking(permutation, visited)
+            //remove
+            permutation.pop()
+            visited[i] = false
+        }
+    }
+    
+    backtracking()
+    
+    return response
+};
