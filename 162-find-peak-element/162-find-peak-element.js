4ef9a9a0ac2007d41d4adf32e86b19952468dfad
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-28 23:57:33 -0400

    Time: 106 ms (12.54%), Space: 42.3 MB (51.03%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    let left = 0
+    let right = nums.length - 1
+    let mid
+    
+    while (left < right) {
+        mid = Math.floor((left + right) / 2)
+        if (nums[mid] > nums[mid + 1]) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    
+    return left
+    
+};
