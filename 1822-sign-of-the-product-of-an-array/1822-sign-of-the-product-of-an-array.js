55ff44ad615785863380d45f26a147c9c464e7bb
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-28 23:08:52 -0400

    Time: 105 ms (16.40%), Space: 44.5 MB (6.34%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -6,10 +6,11 @@ var arraySign = function(nums) {
     let prod = 1
     
     for (let number of nums) {
+        if (!number) return 0
         prod *= number
     }
     
-    return signFunc(prod)   
+    return prod > 0 ? 1 : -1
     
 };
 
