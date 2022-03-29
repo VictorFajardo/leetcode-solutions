ad293cf63e5593b42a20588677f407015dda2ce7
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-03-29 00:07:08 -0400

    Time: 108 ms (64.79%), Space: 45.5 MB (52.54%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -5,22 +5,12 @@
 var numIslands = function(grid) {
     const rows = grid.length
     const cols = grid[0].length
-    
     let total = 0
-    
-    const fill = function(i, j) {
-        grid[i][j] = '0'
-
-        if (i - 1 >= 0 && grid[i - 1][j] === '1') fill(i - 1, j)
-        if (j + 1 < cols && grid[i][j + 1] === '1') fill(i, j + 1)
-        if (i + 1 < rows && grid[i + 1][j] === '1') fill(i + 1, j)
-        if (j - 1 >= 0 && grid[i][j - 1] === '1') fill(i, j - 1)
-    }
     
     grid.forEach((row, i) => {
         row.forEach((col, j) => {
             if (grid[i][j] === '1') {
-                fill(i, j)
+                fill(grid, i, j, rows, cols)
                 total++
             }
         })
@@ -29,3 +19,12 @@ var numIslands = function(grid) {
     return total
 };
 
+
+var fill = function(grid, i, j, rows, cols) {
+    grid[i][j] = '0'
+
+    if (i - 1 >= 0 && grid[i - 1][j] === '1') fill(grid, i - 1, j, rows, cols)
+    if (j + 1 < cols && grid[i][j + 1] === '1') fill(grid, i, j + 1, rows, cols)
+    if (i + 1 < rows && grid[i + 1][j] === '1') fill(grid, i + 1, j, rows, cols)
+    if (j - 1 >= 0 && grid[i][j - 1] === '1') fill(grid, i, j - 1, rows, cols)
+}
