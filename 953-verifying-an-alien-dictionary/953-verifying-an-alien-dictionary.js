ccf5f28b9b37cb39c80af65672d9a99cc2485fd2
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-04 23:27:23 -0400

    Time: 87 ms (41.71%), Space: 44.8 MB (5.75%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} words
+ * @param {string} order
+ * @return {boolean}
+ */
+var isAlienSorted = function(words, order) {
+    const lexical = {}
+    
+    for (let i = 0; i < order.length; i++) {
+        lexical[order[i]] = i
+    }
+    
+    // console.log(lexical)
+
+    for (let i = 1; i < words.length; i++) {
+        let prev = words[i - 1]
+        let curr = words[i]
+        // console.log(prev, curr)
+        for (let j = 0; j < Math.min(prev.length, curr.length); j++) {
+            if (lexical[prev[j]] < lexical[curr[j]]) break
+            else if (lexical[prev[j]] > lexical[curr[j]]) return false
+        }
+        if (prev.length > curr.length && prev.substr(0, curr.length) === curr) return false
+    }
+    
+    return true
+};
