2855630edfb479194d7b3f72d462aebf7661d3fa
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-06 01:24:03 -0400

    Time: 118 ms (40.56%), Space: 45.9 MB (26.33%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.js
+++ 566-reshape-the-matrix/566-reshape-the-matrix.js
@@ -9,12 +9,16 @@ var matrixReshape = function(mat, r, c) {
     const n = mat[0].length
     if (m * n !== r * c) return mat
     
-    const newMat = new Array(r).fill(0).map(_ => new Array(c))
+    const response = new Array(r).fill(0).map(_ => new Array(c))
     
     for (let i = 0; i < m * n; i++) {
         const value = mat[Math.floor(i / n)][i % n]
-        newMat[Math.floor(i / c)][i % c] = value
+        response[Math.floor(i / c)][i % c] = value
     }
     
-    return newMat    
+    return response   
 };
+
+// Array
+// time complexity: O(m * n)
+// space complaxity: O(m * n)
