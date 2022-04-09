05a918b76d4a9d04c2275c5dce0657ac3be4355d
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-09 00:05:43 -0400

    Time: 414 ms (29.57%), Space: 83.9 MB (6.74%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -4,18 +4,23 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    const minPQ = new MinPriorityQueue()
+    const maxPQ = new MaxPriorityQueue()
     const response = []
-    const p0 = [0, 0]
     
     for (let point of points) {
         const ditanceToOrigin = euclideanDistanceToOrigen(point)
-        minPQ.enqueue(point, ditanceToOrigin)
+        if (maxPQ.size() < k) {
+            maxPQ.enqueue(point, ditanceToOrigin)
+        } else {
+            if (ditanceToOrigin < maxPQ.front().priority) {
+                maxPQ.dequeue()
+                maxPQ.enqueue(point, ditanceToOrigin)
+            }
+        }
     }
     
-    while (k > 0) {
-        response.push(minPQ.dequeue().element)
-        k--
+    while (!maxPQ.isEmpty()) {
+        response.push(maxPQ.dequeue().element)
     }
     
     return response
@@ -25,3 +30,7 @@ var euclideanDistanceToOrigen = function(point) {
     const [x, y] = point
     return Math.sqrt(x**2 + y**2)
 }
+
+// Heap
+// time complexity: O(n*log(k))
+// space complexity: O(k)
