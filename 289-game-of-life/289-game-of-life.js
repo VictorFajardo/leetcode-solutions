c9f174aaea1b39a7817936bc1e2dcc296c6904ac
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-12 00:52:00 -0400

    Time: 102 ms (23.79%), Space: 42.2 MB (76.65%) - LeetHub

--- 289-game-of-life/289-game-of-life.js
+++ 289-game-of-life/289-game-of-life.js
@@ -11,18 +11,16 @@ var gameOfLife = function(board) {
             const cellLivingNeihbors = livingNeighbors(board, i, j, m, n)
             const cell = board[i][j]
             if (cell === 0) {
-                if(cellLivingNeihbors === 3) board[i][j] = -1
+                if(cellLivingNeihbors === 3) board[i][j] = 2
             } else if (cell === 1) {
-                if(cellLivingNeihbors < 2 || cellLivingNeihbors > 3) board[i][j] = 2
+                if(cellLivingNeihbors < 2 || cellLivingNeihbors > 3) board[i][j] = -1
             }
         }
     }
     
     for (let i = 0; i < m; i++) {
         for (let j = 0; j < n; j++) {
-            const cell = board[i][j]
-            if (cell === -1) board[i][j] = 1
-            else if (cell === 2) board[i][j] = 0
+            board[i][j] = board[i][j] > 0 ? 1 : 0
         }
     }
     
@@ -33,16 +31,15 @@ var livingNeighbors = function(board, i, j, m, n) {
     let left = Math.max(j - 1, 0)
     let bottom = Math.min(i + 1, m - 1)
     let right = Math.min(j + 1, n - 1)
-    let counter = 0
+    let neighbors = 0
     
     for (let r = top; r <= bottom; r++) {
         for (let c = left; c <= right; c++) {
-            if (r === i && c === j) continue
-            if (board[r][c] >= 1) counter += 1
+            if (!(r === i && c === j) && (Math.abs(board[r][c]) === 1)) neighbors++
         }
     }
     
-    return counter
+    return neighbors
 }
 
 /*
