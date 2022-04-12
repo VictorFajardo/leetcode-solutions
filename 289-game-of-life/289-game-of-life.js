7213bf7153db3ed6670f4ad826e23f57cf5fb3d3
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-12 00:55:41 -0400

    Time: 97 ms (31.50%), Space: 42.5 MB (49.34%) - LeetHub

--- 289-game-of-life/289-game-of-life.js
+++ 289-game-of-life/289-game-of-life.js
@@ -3,42 +3,50 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var gameOfLife = function(board) {
-    const dx = [1, 1, 0, -1, -1, -1, 0, 1];
-    const dy = [0, 1, 1, 1, 0, -1, -1, -1];
+    const m = board.length
+    const n = board[0].length
     
-    const row = board.length;
-    const col = board[0].length;
-    
-    for (let i = 0; i< row; i++) {
-        for (let j = 0; j < col; j++) {
-            let liveCount = 0;
-            for (let k = 0; k < 8; k++) {
-            
-                if (isSafe(row, col, i + dx[k], j+ dy[k]) && Math.abs(board[i + dx[k]][j+ dy[k]]) === 1) {
-                    liveCount++;
-                }
-            
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            const cellLivingNeihbors = livingNeighbors(board, i, j, m, n)
+            const cell = board[i][j]
+            if (cell === 0) {
+                if(cellLivingNeihbors === 3) board[i][j] = 2
+            } else if (cell === 1) {
+                if(cellLivingNeihbors < 2 || cellLivingNeihbors > 3) board[i][j] = -1
             }
-            
-            if (board[i][j] === 0 && liveCount === 3) {
-                    board[i][j] = 2;
-            } else if (board[i][j] === 1 && (liveCount > 3 || liveCount < 2)) {
-                    board[i][j] = -1;
-            }
-            
         }
     }
     
-    for (let i = 0; i < row; i++) {
-        for (let j = 0; j < col; j++) {
-            board[i][j] = (board[i][j] > 0)  ? 1 : 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            board[i][j] = board[i][j] > 0 ? 1 : 0
         }
     }
+    
 };
 
-const isSafe = (row, col,i, j) => {
-    if (i < 0 || i >= row || j < 0 || j >= col) {
-        return false;
+var livingNeighbors = function(board, i, j, m, n) {
+    let top = Math.max(i - 1, 0)
+    let left = Math.max(j - 1, 0)
+    let bottom = Math.min(i + 1, m - 1)
+    let right = Math.min(j + 1, n - 1)
+    let neighbors = 0
+    
+    for (let r = top; r <= bottom; r++) {
+        for (let c = left; c <= right; c++) {
+            if (!(r === i && c === j) && (Math.abs(board[r][c]) === 1)) neighbors++
+        }
     }
-    return true;
+    
+    return neighbors
 }
+
+/*
+(n < 2) dies
+(n >= 2 && n <= 3) lives
+(n > 3) dies
+
+
+
+*/
