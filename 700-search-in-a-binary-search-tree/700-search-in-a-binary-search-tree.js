f4049ed7793e86b64f909cf3dff0c79567ddfe80
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-13 23:31:36 -0400

    Time: 90 ms (75.51%), Space: 49.8 MB (15.23%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.js
@@ -19,10 +19,19 @@ var searchBST = function(root, val) {
     return root
 };
 
+// Iteration
+// time complexity: O(log(n))
+// space complexity: O(1)
+
 /*
 var searchBST = function(root, val) {
     if (!root || root.val === val) return root
     
     return root.val > val ? searchBST(root.left, val) : searchBST(root.right, val)    
 };
+
+// Recursion
+// time complexity: O(log(n))
+// space complexity: O(1)
 */
+
