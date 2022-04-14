8ea6e02ef18d036f980009ba542287a35931a9a8
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-14 18:18:01 -0400

    Time: 100 ms (74.06%), Space: 47.7 MB (63.28%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.js
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.js
@@ -5,24 +5,20 @@
  */
 var kthSmallest = function(matrix, k) {
     const n = matrix.length
-    const minPQ = new MinPriorityQueue()
-    // const response = []
+    const minPQ = new MinPriorityQueue({ compare: (a, b) => a.value - b.value })
     
     matrix.map((row, index) => {
-        minPQ.enqueue({rowIndex: index, numIndex: 0}, row[0])
+        minPQ.enqueue({ rowIdx: index, colIdx: 0, value: row[0] })
     })
     
     while (k > 0) {
-        let { priority, element: {rowIndex, numIndex} } = minPQ.dequeue()
-        if (k === 1) return priority
-        // response.push(priority)
-        if (numIndex < matrix[rowIndex].length - 1) {
-            const row = matrix[rowIndex]
-            minPQ.enqueue({rowIndex: rowIndex, numIndex: numIndex + 1}, row[numIndex + 1])
+        let { rowIdx, colIdx, value } = minPQ.dequeue()
+        if (k === 1) return value
+        const row = matrix[rowIdx]
+        if (colIdx < row.length - 1) {
+            minPQ.enqueue({ rowIdx: rowIdx, colIdx: colIdx + 1, value: row[colIdx + 1] })
         }
         k--
     }
-    
-    // console.log(minPQ.toArray())
     
 };
