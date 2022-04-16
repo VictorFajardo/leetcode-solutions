97df84f02e9b39f3bbd47719c28c1378a395dee9
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-15 23:20:55 -0400

    Time: 78 ms (91.83%), Space: 44.5 MB (98.53%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    const graph = new Array(numCourses).fill(0).map(() => new Array())
+    const inDegree = new Array(numCourses).fill(0)
+    const queue = new Queue()
+    let counter = 0
+    
+    prerequisites.forEach(( [child, parent] ) => {
+        graph[parent].push(child)
+        inDegree[child]++
+    })
+    
+    inDegree.forEach((edge, index) => {
+        if (edge === 0) queue.enqueue(index)
+    })
+    
+    while (!queue.isEmpty()) {
+        let course = queue.dequeue()
+        counter++
+        let children = graph[course]
+        children.forEach((child) => {
+            inDegree[child]--
+            if (inDegree[child] === 0) queue.enqueue(child)
+        })
+    }
+    
+    return counter === numCourses
+};
