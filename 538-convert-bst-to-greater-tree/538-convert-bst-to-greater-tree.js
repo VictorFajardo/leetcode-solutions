54c533df06d5f605a6d3a01360d73735634ff1e7
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-15 21:48:34 -0400

    Time: 93 ms (88.89%), Space: 51.9 MB (46.83%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function(root) {
+    if (!root) return root
+    let sum = 0
+    
+    var helper = function(root) {
+        if (!root) return
+
+        helper (root.right)
+        sum += root.val
+        root.val = sum
+        helper (root.left)
+        
+        return root
+    }
+    
+    return helper(root)
+    
+};
+
