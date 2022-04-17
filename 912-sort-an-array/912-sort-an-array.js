b01f029ff9536acd642b038c906965ea77cb7c7a
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-16 23:52:47 -0400

    Time: 4082 ms (21.36%), Space: 58 MB (47.98%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -3,48 +3,69 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-
-    return mergeSort(nums)
+    // return mergeSort(nums)
+    return quickSort(nums)
 };
 
+// QuickSort
+var quickSort = function(arr, start = 0, end = arr.length - 1) {
+    if (start < end) {
+        let pivot = getPivot(arr, start, end)
+        quickSort(arr, start, pivot - 1)
+        quickSort(arr, pivot + 1, end)
+    }
+    return arr
+}
+
+var getPivot = function(arr, start, end) {
+    let pivot = arr[start]
+    let swapIndex = start
+    
+    for (let i = start + 1; i <= end; i++) {
+        if (pivot > arr[i]) {
+            swapIndex++
+            [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]]
+        }
+    }
+    
+    [arr[swapIndex], arr[start]] = [arr[start], arr[swapIndex]]
+    return swapIndex
+}
+
+//Merge Sort
 var mergeSort = function(array) {
-    if (array.length == 1) return array
-    
-    //divide the array in 2 halves
-    let mid = Math.floor(array.length / 2)
-    
+    if (array.length === 1) return array
+    const mid = Math.floor(array.length / 2)
     const left = array.slice(0, mid)
     const right = array.slice(mid)
     
-    return merge(
-        mergeSort(left),
-        mergeSort(right)
-    )
+    return merge(mergeSort(left), mergeSort(right))
 }
 
 var merge = function(left, right) {
-    const res = []
+    const merged = []
     let leftIndex = 0
     let rightIndex = 0
-    while(leftIndex < left.length && rightIndex < right.length) {
+    
+    while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
-            res.push(left[leftIndex])
+            merged.push(left[leftIndex])
             leftIndex++
         } else {
-            res.push(right[rightIndex])
+            merged.push(right[rightIndex])
             rightIndex++
         }
     }
     
     while (leftIndex < left.length) {
-        res.push(left[leftIndex])
-        leftIndex++
+        merged.push(left[leftIndex])
+        leftIndex++        
     }
     
     while (rightIndex < right.length) {
-        res.push(right[rightIndex])
-        rightIndex++
+        merged.push(right[rightIndex])
+        rightIndex++        
     }
     
-    return res
+    return merged
 }
