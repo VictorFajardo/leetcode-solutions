2e395bfdcde227e79697e4ad5fb2f74b9fa51c5e
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-19 14:43:17 -0400

    Time: 1113 ms (24.28%), Space: 51.6 MB (10.42%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -3,21 +3,33 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    
-    
-    var dp = function(n, cache = {}) {
-        if (n === nums.length - 1) return true
-        
-        let range = nums[n]
-        for (let i = n + 1; i <= n + range; i++) {
-            if (!(i in cache)) cache[i] = dp(i, cache)
-            if (cache[i]) return true
-        }
-        
-        return false
-        
+    // Iteration
+    /*const n = nums.length - 1
+    let max = 0
+    for (let i = 0; i < n && i <= max; i++) {
+        max = Math.max(max, i + nums[i])
     }
     
-    return dp(0)
+    return max >= n*/
     
+    // Dynamic Programming    
+    const cache = []
+    const dp = (n, nums, cache) => {
+        if (n === nums.length - 1) return true
+        
+        for (let i = nums[n]; i > 0 ; i--) {
+            if (cache[n + i] === undefined) {
+                cache[n + i] = dp(n + i, nums, cache)
+            }
+            if (cache[n + i]) return true            
+        }
+        
+        return false        
+    }
+    
+    return dp(0, nums, cache)
 };
+
+// Dynamic Programming
+// time complexity: O(n * m)
+// space complexity: O(n)
