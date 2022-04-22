0029dd24e98fda4e96438998667f47b2e1d93afc
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-21 23:35:02 -0400

    Time: 68 ms (67.35%), Space: 42.4 MB (13.91%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -3,41 +3,23 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    
-    // Linked List solution
-    if (!digits) return []
     const keyboard = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
-    const list = [''] //replace for a Linked List
+    const queue = new Queue()
+    const n = digits.length
+    queue.enqueue('')
     
-    while(list[0].length !== digits.length) {
-          const base = list.shift()
-          const chars = keyboard[digits[base.length]]
-          for (let i = 0; i < chars.length; i++) {
-              list.push(base + chars[i])
-          }
-    }
-    
-    return list
-    
-    
-    // Recursive solution
-    /*if (!digits) return []
-    const keyboard = ['', '', 'abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
-    const res = []
-    
-    const conbinations = (digits, index = 0, str = '') => {
-        if (digits.length === index) res.push(str)
-        else {
-            const number = digits[index]
-            const keys = keyboard[number]
-            for (let i = 0; i < keys.length; i++) {
-                conbinations(digits, index + 1, str + keys[i])
-            }    
+    while(queue.front().length < n) {
+        let current = queue.dequeue()
+        let index = current.length
+        let digit = digits[index]
+        let chars = keyboard[digit]
+        
+        for (let char of chars) {
+            queue.enqueue(current + char)
         }
     }
     
-    conbinations(digits)
-    
-    return res*/
+    if (queue.size() === 1) return []
+    return queue.toArray()
     
 };
