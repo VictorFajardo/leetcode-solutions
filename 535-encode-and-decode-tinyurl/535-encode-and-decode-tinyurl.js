4aaa85bd41baa1246f65525ca16edfa414bf17f6
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-23 18:39:54 -0400

    Time: 102 ms (41.35%), Space: 44.5 MB (13.46%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.js
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.js
@@ -3,29 +3,31 @@ const long = new Map()
 const short = new Map()
 const baseUrl = 'http://tinyurl.com/'
 
-var generateKey = function() {
-    let key = ''
+var generateNewKey = function() {
+    let newKey = ''
     for (let i = 0; i < 6; i++) {
-        let random = Math.floor(Math.random() * 62)
-        key += alphabet[random]
+        let randomNumber = Math.floor(Math.random() * 62)
+        newKey += alphabet[randomNumber]
     }
-    return key
+    return newKey
 }
 
 var encode = function(longUrl) {
     if (long.has(longUrl)) {
         return long.get(longUrl)
     }
-    let key = Date.now()
+    let key = generateNewKey()
+    while (long.has(key)) {
+        key = generateNewKey()
+    }
     long.set(longUrl, key)
     short.set(key, longUrl)
     return baseUrl + key
 }
 
 var decode = function(shortUrl) {
-    let key = Number(shortUrl.replace(baseUrl, ''))
+    let key = shortUrl.replace(baseUrl, '')
     if (short.has(key)) {
-        console.log('here')
         return short.get(key)    
     }
     return
