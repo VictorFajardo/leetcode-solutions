7cc2d6a88a6d371f4f6f503eb8e33af2a323856c
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-24 20:47:49 -0400

    Time: 149 ms (24.25%), Space: 45 MB (30.29%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,35 +4,16 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    // const minPQ = new MinPriorityQueue()
-    
-    // time: O(n*log(k))
-    // space: O(k)
-    // nums.map((num) => {
-    //     minPQ.enqueue(num)
-    //     if (minPQ.size() > k) {
-    //         minPQ.dequeue()
-    //     }
-    // })
-    
-    // space: O(1)
-    // time: O(1)
-    // return minPQ.front().element
-    
     const target = nums.length - k
     
     return quickSelect(nums, target)
-    
 };
 
 var quickSelect = function(arr, target, start = 0, end = arr.length - 1) {
-    // if (start < end) {
     let pivot = getPivot(arr, start, end)
     if (pivot === target) return arr[pivot]
     else if (pivot > target) return quickSelect(arr, target, start, pivot - 1)
     else return quickSelect(arr, target, pivot + 1, end)
-    // }
-    // return arr
 }
 
 var getPivot = function(arr, start, end) {
@@ -49,3 +30,31 @@ var getPivot = function(arr, start, end) {
     [arr[swapIndex], arr[start]] = [arr[start], arr[swapIndex]]
     return swapIndex
 }
+
+// Quick Select
+// time complexity: O(n)
+// space complexity: O(n)
+
+/*
+var findKthLargest = function(nums, k) {
+    const minPQ = new MinPriorityQueue()
+    
+    // time: O(n*log(k))
+    // space: O(k)
+    nums.map((num) => {
+        minPQ.enqueue(num)
+        if (minPQ.size() > k) {
+            minPQ.dequeue()
+        }
+    })
+    
+    // space: O(1)
+    // time: O(1)
+    return minPQ.front().element
+    
+};
+*/
+
+// Piority Queue
+// time complexity: O(n*log(k))
+// space complexity: O(k)
