fc81d42a451147d5cd83e17008df6e3cf4eefd17
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-25 22:33:46 -0400

    Time: 478 ms (79.40%), Space: 44.7 MB (92.92%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const m = matrix.length
+    const n = matrix[0].length
+    let row = 0
+    let col = n - 1
+    
+    while (row < m && col >= 0) {
+        if (matrix[row][col] < target) row++
+        else if (matrix[row][col] > target) col--
+        else return true
+    }    
+    
+    return false
+};
