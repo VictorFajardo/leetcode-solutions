fda4b8361b8eccbb788325c4c1c4c1e4699e7f62
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-26 00:28:12 -0400

    Time: 205 ms (36.53%), Space: 46.1 MB (97.93%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const values = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1,
+    }
+    let num = 0
+    let last = 0
+    for (let i = s.length - 1; i >= 0; i--) {
+        let char = s[i]
+        let value = values[char]
+        if (value >= last) num += value
+        else num -= value
+        last = value
+    }
+    
+    return num
+};
