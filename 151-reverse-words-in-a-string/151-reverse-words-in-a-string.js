6f44292f2184808bd9b0c9f6db26bcaa71019119
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-26 22:54:35 -0400

    Time: 112 ms (16.23%), Space: 45 MB (13.61%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
@@ -4,29 +4,17 @@
  */
 var reverseWords = function(s) {
     let response = ''
-    let start = 0
     
-    for (let end = 0; end < s.length; end++) {
-        if (s[end] === ' ') continue
-        start = end
-        while (s[end + 1] !== ' ' && end < s.length) {
-            end++
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') continue
+        let word = ''
+        while (s[i] !== ' ' && i < s.length) {
+            word += s[i]
+            i++
         }
-        response = s.substring(start, end + 1) + (response.length > 0 ? ' ': '') + response
+        response = word + (response.length > 0 ? ' ': '') + response
     }
     
     return response
-    
-    
-    
-//     const words = s.split(' ').reverse()
-//     let response = ''
-    
-//     console.log(words)
-    
-//     words.forEach((word) => {
-//         if (word !== '') response += (response.length > 0 ? ' ': '') + word
-//     })
-    
-//     return response
+
 };
