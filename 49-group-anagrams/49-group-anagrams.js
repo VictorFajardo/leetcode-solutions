3431c68ed71b1fd884edd9c9c5e221808fb4b9cf
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-28 22:56:44 -0400

    Time: 254 ms (16.29%), Space: 55.6 MB (13.78%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const anagramsMap = {}
+    const response = []
+    
+    
+    strs.forEach((word) => {
+        const charMap = new Array(26).fill(0)
+        for (let char of word) {
+            let index = char.charCodeAt(0) - 97
+            charMap[index]++
+        }
+        const key = charMap.join(':')
+        if (!(key in anagramsMap)) anagramsMap[key] = []
+        anagramsMap[key].push(word)
+    })
+    
+    for (let words of Object.values(anagramsMap)) {
+        response.push(words)
+    }
+    
+    return response
+};
+
+// Array
+// time complexity: O(nk)
+// space complexity: O(nk)
