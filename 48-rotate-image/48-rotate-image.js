57c9afa28ce945d18d8486b51c99689850d82556
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-28 23:24:25 -0400

    Time: 146 ms (5.04%), Space: 44 MB (10.58%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    const n = matrix.length
+    
+    const transpose = (matrix) => {
+        for (let row = 0; row < n - 1; row++) {
+            for (let col = row + 1; col < n; col++) {
+                [matrix[row][col], matrix[col][row]] = [matrix[col][row], matrix[row][col]]
+            }
+        }
+    }
+    
+    const reverse = (matrix) => {
+        for (let row = 0; row < n; row++) {
+            for (let col = 0; col < Math.floor(n / 2); col++) {
+                [matrix[row][col], matrix[row][n - col - 1]] = [matrix[row][n - col - 1], matrix[row][col]]
+            }
+        }
+    }
+    
+    transpose(matrix)
+    
+    reverse(matrix)
+    
+};
