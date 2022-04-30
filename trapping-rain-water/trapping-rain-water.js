aea23ce524f94e2ed9988c169fb9348d96f72698
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-29 23:50:59 -0400

    Time: 93 ms, Memory: 43.2 MB - LeetHub

---
+++ trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0
+    let right = height.length - 1
+    let maxLeft = 0
+    let maxRight = 0
+    let trappingWater = 0
+    
+    while (left < right) {
+        if (height[left] < height[right]) {
+            maxLeft = Math.max(maxLeft, height[left])
+            trappingWater += maxLeft - height[left]
+            left++
+        } else {
+            maxRight = Math.max(maxRight, height[right])
+            trappingWater += maxRight - height[right]
+            right--
+        }
+    }
+    
+    return trappingWater
+    
+};
+
+// Two Pointers
+// time complexity O(n)
+// space complexity O(1)
