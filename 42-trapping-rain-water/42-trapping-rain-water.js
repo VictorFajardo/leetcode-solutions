4017e05b8748f3327163b269874d126952c6005b
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-29 23:49:05 -0400

    Time: 76 ms (75.63%), Space: 43 MB (70.46%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -3,24 +3,30 @@
  * @return {number}
  */
 var trap = function(height) {
-    let left = 0
-    let right = height.length - 1
-    let maxLeft = 0
-    let maxRight = 0
-    let trappingWater = 0
+    let water = 0
+    let p1 = 0
+    let p2 = height.length - 1
+    let maxLeft = height[p1]
+    let maxRight = height[p2]
+    let diff = 0
     
-    while (left < right) {
-        if (height[left] < height[right]) {
-            maxLeft = Math.max(maxLeft, height[left])
-            trappingWater += maxLeft - height[left]
-            left++
+    while (p1 < p2) {
+        let minTop = Math.min(maxLeft, maxRight)
+        if (height[p1] < height[p2]) {
+            diff = minTop - height[p1]
+            p1++
+            maxLeft = Math.max(maxLeft, height[p1])
         } else {
-            maxRight = Math.max(maxRight, height[right])
-            trappingWater += maxRight - height[right]
-            right--
+            diff = minTop - height[p2]
+            p2--
+            maxRight = Math.max(maxRight, height[p2])
         }
+        if (diff > 0) water += diff
     }
     
-    return trappingWater
-    
+    return water
 };
+
+// 2 Pointers
+// time complexity O(n)
+// space complexity O(1)
