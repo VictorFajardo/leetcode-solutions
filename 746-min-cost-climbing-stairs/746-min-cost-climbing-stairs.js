e92b0ee3d02c303ec0a404b623f536a38f3dea8b
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-01 17:30:36 -0400

    Time: 88 ms (50.77%), Space: 43.9 MB (59.86%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -9,10 +9,10 @@ var minCostClimbingStairs = function(cost) {
     var dp = function(n, cache) {
         if (n < 2) return 0
         
-        if (!((n - 2) in cache)) cache[n - 2] = dp(n - 2, cache)
-        if (!((n - 1) in cache)) cache[n - 1] = dp(n - 1, cache)
+        if (!((n - 2) in cache)) cache[n - 2] = dp(n - 2, cache) + cost[n - 2]
+        if (!((n - 1) in cache)) cache[n - 1] = dp(n - 1, cache) + cost[n - 1]
         
-        return Math.min(cache[n - 1] + cost[n - 1], cache[n - 2] + cost[n - 2])
+        return Math.min(cache[n - 1], cache[n - 2])
     }
     
     return dp(n, cache)
