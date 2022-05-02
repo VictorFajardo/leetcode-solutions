ddbe6b8c4cce3a70899996c7b68c1dcf56039193
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-01 21:56:58 -0400

    Time: 76 ms (49.52%), Space: 41.5 MB (91.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,15 +2,36 @@
  * @param {number} n
  * @return {number}
  */
-var climbStairs = function(n, store = {}) {
-    if (n === 2) return 2
-    if (n === 1) return 1
-    if (n < 1) return 0
+var climbStairs = function(n) {
+    // recursive
+    /*const options = 2
+    const cache = {}
     
-    if (!((n - 1) in store)) store[n - 1] = climbStairs(n - 1, store)
-    if (!((n - 2) in store)) store[n - 2] = climbStairs(n - 2, store)
+    const maxSteps = step => {
+        if (step < 0) return
+        else if (step <= options) return step
+        if (cache[step]) return cache[step]
+        cache[step] = maxSteps(step - 1) + maxSteps(step - 2)
+        return cache[step]
+    }
     
-    let max = store[n - 1] + store[n - 2]
+    return maxSteps(n)*/
     
-    return max
+    // iteration
+    if (n <=2) return n
+    // const res = [1, 2]
+    let oneStepBefore = 2
+    let twoStepBefore = 1
+    let total = 0
+    for (let i = 2; i < n; i++) {
+        total = oneStepBefore + twoStepBefore
+        twoStepBefore = oneStepBefore
+        oneStepBefore = total
+    }
+    return total
 };
+
+// dynamic programming + memoization
+// time complexity O(n)
+// space complexity O(n)
+
