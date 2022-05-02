9d17723a9c069ae8a7a61973419f0b29ed36202c
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-01 22:09:46 -0400

    Time: 109 ms (43.03%), Space: 44.6 MB (58.44%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -4,12 +4,14 @@
  */
 var sortArrayByParity = function(nums) {
     let start = 0
+    let end = nums.length - 1
     
-    for (let end = 0; end < nums.length; end++) {
-        if (nums[end] % 2 === 0) {
+    while (start < end) {
+        if (nums[start] % 2 > nums[end] % 2) {
             [nums[start], nums[end]] = [nums[end], nums[start]]
-            start++
         }
+        if (nums[start] % 2 === 0) start += 1
+        if (nums[end] % 2 === 1) end -= 1
     }
     
     return nums
