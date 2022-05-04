c58e9da94ea56782c45a2b0504f1aaeb5a785b5d
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-03 23:18:45 -0400

    Time: 90 ms (90.07%), Space: 44.1 MB (96.32%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.js
+++ 277-find-the-celebrity/277-find-the-celebrity.js
@@ -14,33 +14,29 @@
  * @return {function}
  */
 var solution = function(knows) {
+    function isCandidate(i, n) {
+        for (let j = 0; j < n; j++) {
+            if (i === j) continue
+            if (knows(i, j) || !knows(j, i)) return false
+        }
+        return true
+    }
     
     /**
      * @param {integer} n Total people
      * @return {integer} The celebrity
      */
     return function(n) {
-        const popularity = new Array(n).fill(0)
-        const candidates = []
-    
-        for (let i = 0; i < n; i++) {
-            let people = 0
-            for (let j = 0; j < n; j++) {
-                if (i === j) continue
-                if (knows(i, j)) {
-                    popularity[j] += 1
-                    people += 1
-                }
-            }
-            if (people === 0) candidates.push(i)
-        }
-        console.log(candidates, n)
-        console.log(popularity)
+        let candidate = 0
         
-        for (let i = 0; i < candidates.length; i++) {
-            const candidate = candidates[i]
-            if (popularity[candidate] === n - 1) return candidate
+        for (let i = 0; i < n; i++) {
+            if (knows(candidate, i)) {
+                candidate = i
+            }
         }
+        
+        if (isCandidate(candidate, n)) return candidate
         return -1
+        
     };
 };
