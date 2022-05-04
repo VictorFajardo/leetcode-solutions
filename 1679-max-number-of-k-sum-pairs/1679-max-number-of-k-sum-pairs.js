ff0072e6db7f8f2fed82c39937c9e67c541ed3c8
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-03 20:29:12 -0400

    Time: 297 ms (16.98%), Space: 58.8 MB (30.19%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var maxOperations = function(nums, k) {
+    const store = {}
+    let pairs = 0
+    
+    nums.forEach((number) => {
+        let complement = k - number
+        if (complement in store) {
+            store[complement] -= 1
+            if (store[complement] === 0) {
+                delete store[complement]
+            }
+            pairs += 1
+        } else {
+            if (!(number in store)) store[number] = 0
+            store[number] += 1
+        }
+    })
+    
+    return pairs
+};
+
+/*
+First Solution:
+create a hashmap to storage the complementys of any number in the array
+if a number === complement, we found a pair, we remove the complement from the storage
+
+Time O(n)
+Space O(n)
+
+Second Solution:
+sort the array, create 2 pointers on the sides, and move to the center looking for pairs that sum K
+
+Time O(nlogn) + O(n) = O(n*log(n))
+Space O(1)
+
+*/
