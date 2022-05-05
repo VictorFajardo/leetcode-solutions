bfba53d588a653022f650a83af11bc4f7f8a3993
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-04 23:05:52 -0400

    Time: 99 ms (87.34%), Space: 47.1 MB (62.03%) - LeetHub

---
+++ 484-find-permutation/484-find-permutation.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var findPermutation = function(s) {
+    const stack = []
+    const res = []
+    let n = 1
+    
+    for (let i = 0; i <= s.length; i++) {
+        stack.push(n)
+        const char = s.charAt(i)
+        if (char === 'I' || i === s.length) {
+            while(stack.length) {
+                res.push(stack.pop())
+            }
+        }
+        n += 1
+    }
+    
+    return res
+    
+};
