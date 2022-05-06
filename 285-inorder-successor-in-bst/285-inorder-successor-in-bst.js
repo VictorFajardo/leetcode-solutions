7c4de0e6fc06a1183076c99f7d3273cc86bb1491
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-05 22:07:16 -0400

    Time: 104 ms (60.21%), Space: 52.1 MB (63.87%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function(root, p) {
+    if (!root) return
+    
+    if (root.val <= p.val) {
+        return inorderSuccessor(root.right, p)
+    } else {
+        const left = inorderSuccessor(root.left, p)    
+        return left ? left : root
+    }
+    
+
+    
+};
