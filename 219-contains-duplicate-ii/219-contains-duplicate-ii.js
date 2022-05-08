fe960da8d9476972ff00f64f960dbe80a69c6ed1
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-08 01:36:42 -0400

    Time: 198 ms (39.51%), Space: 55.9 MB (82.79%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.js
@@ -4,15 +4,28 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    const map = new Map()
+    const set = new Set()
     
     for (let index = 0; index < nums.length; index++) {
         const number = nums[index]
-        if (map.has(number)) {
-            if (index - map.get(number) <= k) return true
+        if (set.has(number)) return true
+        set.add(number)
+        if (set.size > k) {
+            set.delete(nums[index - k])
         }
-        map.set(number, index)
     }
     
     return false
+    
+//     const map = new Map()
+    
+//     for (let index = 0; index < nums.length; index++) {
+//         const number = nums[index]
+//         if (map.has(number)) {
+//             if (index - map.get(number) <= k) return true
+//         }
+//         map.set(number, index)
+//     }
+    
+//     return false
 };
