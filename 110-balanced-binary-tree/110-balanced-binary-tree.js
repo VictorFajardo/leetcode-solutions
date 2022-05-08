a29660e771b9612f84cafdadc898c3917d418081
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-08 02:46:08 -0400

    Time: 133 ms (16.44%), Space: 47.4 MB (41.85%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+    if (!root) return true
+    
+    if (Math.abs(height(root.left) - height(root.right)) > 1) return false
+    else return isBalanced(root.left) && isBalanced(root.right)
+};
+
+var height = function(root) {
+    if (!root) return -1
+    
+    return 1 + Math.max(height(root.left), height(root.right))
+}
