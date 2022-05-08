cfc282cec24f166f222b910bc6fcca765c2bf04a
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-07 23:29:43 -0400

    Time: 178 ms (78.42%), Space: 51.2 MB (59.80%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.js
@@ -6,13 +6,10 @@ var KthLargest = function(k, nums) {
     this.k = k
     this.pq = new MinPriorityQueue()
     
-    for (let el of nums) {
-        this.pq.enqueue(el)
-    }
+    nums.forEach((val) => {
+        this.add(val)
+    })
     
-    while (this.pq.size() > this.k) {
-        this.pq.dequeue()
-    }
 };
 
 /** 
@@ -20,10 +17,14 @@ var KthLargest = function(k, nums) {
  * @return {number}
  */
 KthLargest.prototype.add = function(val) {
-    this.pq.enqueue(val)
-    if (this.pq.size() > this.k) {
-        this.pq.dequeue()
+    if (this.pq.size() < this.k) this.pq.enqueue(val)
+    else {
+        if (val > this.pq.front().element) {
+            this.pq.enqueue(val)
+            this.pq.dequeue()
+        }
     }
+    
     return this.pq.front().element
 };
 
