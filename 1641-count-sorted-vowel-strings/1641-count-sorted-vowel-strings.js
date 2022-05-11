37df8c7c2624001b922599ac846df29aa6278c06
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-10 23:41:42 -0400

    Time: 118 ms (34.46%), Space: 41.9 MB (70.95%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var countVowelStrings = function(n) {
+var countVowelStrings = function(n, v = 5) {
     
     /*
     * Combination : k = (k + n - 1)!
@@ -12,7 +12,12 @@ var countVowelStrings = function(n) {
     *          4!n!         4!n!                              24
     */
     
-    return (n + 1)*(n + 2)*(n + 3)*(n + 4)/24
+    //return (n + 1)*(n + 2)*(n + 3)*(n + 4)/24
+    if (n === 1)
+        return v
+    if (v === 1)
+        return 1
+    return countVowelStrings(n, v - 1) + countVowelStrings(n - 1, v)
     
     
     
