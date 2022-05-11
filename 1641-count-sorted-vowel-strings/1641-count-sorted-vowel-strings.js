e414e6b7b9398ff8ad50e99011505ab3a27d992b
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-10 23:51:59 -0400

    Time: 91 ms (50.68%), Space: 44.1 MB (21.62%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
-var countVowelStrings = function(n, v = 5) {
+var countVowelStrings = function(n, v = 5, cache = new Map()) {
     
     /*
     * Combination : k = (k + n - 1)!
@@ -17,8 +17,13 @@ var countVowelStrings = function(n, v = 5) {
         return v
     if (v === 1)
         return 1
-    return countVowelStrings(n, v - 1) + countVowelStrings(n - 1, v)
     
+    let key1 = `${n}:${v-1}`
+    let key2 = `${n-1}:${v}`
     
+    if (!cache.has(key1)) cache.set(key1, countVowelStrings(n, v - 1, cache))
+    if (!cache.has(key2)) cache.set(key2, countVowelStrings(n - 1, v, cache))
+    
+    return cache.get(key1) + cache.get(key2)
     
 };
