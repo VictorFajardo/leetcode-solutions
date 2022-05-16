ab451f4ad48d4a9478a7059eb932d9c353acb8d9
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-16 00:19:35 -0400

    Time: 212 ms (75.55%), Space: 63.3 MB (53.05%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.js
@@ -26,7 +26,7 @@ class TrieNode {
 }
 
 var Trie = function() {
-    this.root = {}
+    this.root = new Map()
 };
 
 /** 
@@ -36,10 +36,10 @@ var Trie = function() {
 Trie.prototype.insert = function(word) {
     node = this.root
     for (let char of word) {
-        if (node[char] === undefined) node[char] = {}
-        node = node[char]
+        if (!node.has(char)) node.set(char, new Map())
+        node = node.get(char)
     }
-    node.isEnd = true
+    node.set('isEnd', true)
 };
 
 /** 
@@ -48,7 +48,7 @@ Trie.prototype.insert = function(word) {
  */
 Trie.prototype.search = function(word) {
     let node = this.startsWith(word)
-    return node && node.isEnd === true
+    return node && node.has('isEnd')
     
 };
 
@@ -59,8 +59,8 @@ Trie.prototype.search = function(word) {
 Trie.prototype.startsWith = function(prefix) {
     node = this.root
     for (let char of prefix) {
-        if (node[char] === undefined) return false
-        node = node[char]
+        if (!node.has(char)) return false
+        node = node.get(char)
     }
     return node
     
