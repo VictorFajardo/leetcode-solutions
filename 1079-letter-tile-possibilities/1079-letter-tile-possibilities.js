dbdec41152ea1dddd52a63fa1d24f6c943760d6b
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-18 00:01:10 -0400

    Time: 79 ms (89.76%), Space: 48.3 MB (58.27%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.js
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.js
@@ -3,27 +3,17 @@
  * @return {number}
  */
 var numTilePossibilities = function(tiles) {
-    const n = tiles.length
-    const set = new Set()
-    
-    const bt = function(str, seq = '', visited = new Set()) {
-        //base case
-        if (seq.length) set.add(seq)
-        if (seq.length === n) return        
+    const visited = new Set()
+    let count = 0
         
-        for (let i = 0; i < n; i++) {
-            //skip visited indexes
-            if (visited.has(i)) continue
-            
-            const char = str.charAt(i)
-            visited.add(i)
-            bt(str, seq + char, visited)
-            visited.delete(i)
-        }
+    for (let i = 0; i < tiles.length; i++) {
+        let char = tiles.charAt(i)
+        //skip visited characters
+        if (visited.has(char)) continue
+        
+        count += numTilePossibilities(tiles.substring(0,i) + tiles.substring(i + 1)) + 1
+        visited.add(char)
     }
     
-    bt(tiles)
-    
-    return set.size
-    
+    return count
 };
