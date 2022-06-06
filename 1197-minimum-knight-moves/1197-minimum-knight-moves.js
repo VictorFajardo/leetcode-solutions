d2c152cf73ae822e122335ee6d888158cd7d232a
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-06-06 15:36:37 -0400

    Time: 1349 ms (60.22%), Space: 101.1 MB (46.24%) - LeetHub

---
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minKnightMoves = function(x, y) {
+    if (x === 0 && y === 0) return 0
+    x = Math.abs(x)
+    y = Math.abs(y)
+    const directions = [[2,1],[1,2],[-1,2],[-2,1],[-2,-1],[-1,-2],[1,-2],[2,-1]]
+    const queue = new Queue()
+    queue.enqueue([0,0])
+    const visited = new Set()
+    visited.add('0:0')
+    let steps = 0
+    
+    while (!queue.isEmpty()) {
+        let qSize = queue.size()
+        while (qSize > 0) {
+            let [row, col] = queue.dequeue()            
+            if (row === x && col === y) return steps
+            for (let [newRow, newCol] of directions) {
+                newRow += row
+                newCol += col
+                if (!visited.has(`${newRow}:${newCol}`) && newRow >= -1 && newCol >= -1) {
+                    queue.enqueue([newRow, newCol])
+                    visited.add(`${newRow}:${newCol}`)
+                }
+            }    
+            qSize -= 1
+        }
+        steps += 1
+    }
+    
+    // console.log(visited)
+    return -1
+    
+};
+
