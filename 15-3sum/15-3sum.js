a9a5a3aeda97ac791fe965b18213e79134efe2c1
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-06-09 13:50:31 -0400

    Time: 232 ms (40.46%), Space: 52.4 MB (73.45%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,33 +3,37 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b)
+    const end = nums.length - 1
     const response = []
+    nums.sort((a, b) => a - b)
     
-    for (let start = 0; start < nums.length - 2; start++) {
-        if (start > 0 && nums[start] === nums[start - 1]) continue
-        getPairs(nums, start + 1, response)
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue
+        }
+        getPairs(nums, i + 1, end, -nums[i], response)
     }
     
     return response
 };
 
-var getPairs = function(arr, start, response) {
-    let target = -arr[start - 1]
-    let end = arr.length - 1
-    
+var getPairs = function(arr, start, end, target, response) {
     while (start < end) {
-        if (arr[start] + arr[end] > target) {
-            end--
-        } else if (arr[start] + arr[end] < target) {
-            start++
-        } else {
+        let sum = arr[start] + arr[end]
+        if (sum === target) {
             response.push([-target, arr[start], arr[end]])
-            end--
             start++
-            while(start < end && arr[end] === arr[end + 1]) end--
-            while(start < end && arr[start] === arr[start - 1]) start++
+            end--
+            while (start < end && arr[start] === arr[start - 1]) start++
+            while (start < end && arr[end] === arr[end + 1]) end--
+        } else if (sum > target) {
+            end--
+        } else if (sum < target) {
+            start++
         }
     }
-    
 }
+
+// Two Pointers
+// time complexity: O(n)
+// space complexity: O(n)
