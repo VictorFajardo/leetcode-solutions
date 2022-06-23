4177182974d064b08aa973c41d8d159e85fb3a8f
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-06-22 22:16:28 -0400

    Time: 116 ms (63.06%), Space: 45.6 MB (31.72%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.ts
+++ 724-find-pivot-index/724-find-pivot-index.ts
@@ -1,13 +1,15 @@
 function pivotIndex(nums: number[]): number {
-    let leftSum = 0
-    let sum = 0
+    let leftSum: number = 0;
+    let sum: number = 0;
     
-    for (let number of nums) sum += number
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (leftSum === sum - leftSum - nums[i]) return i
-        leftSum += nums[i]
+    for (let num of nums) {
+        sum += num;
     }
     
-    return -1
+    for (let i: number = 0; i < nums.length; i++) {
+        if (leftSum === sum - leftSum - nums[i]) return i;
+        leftSum += nums[i];
+    }
+    
+    return -1;
 };
