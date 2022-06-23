726d3bf392f63c07c0749d862be479993fa25f34
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-06-22 20:28:12 -0400

    Time: 116 ms (11.25%), Space: 42.8 MB (75.00%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.ts
@@ -0,0 +1,20 @@
+function wordPattern(pattern: string, s: string): boolean {
+    const arr = s.split(' ')
+    
+    if (arr.length !== pattern.length) return false
+    
+    const map = new Map()
+    const set = new Set()
+    
+    
+    for (let i = 0; i < arr.length; i++) {
+        const word = arr[i]
+        const letter = pattern[i]
+        if (!map.has(word) && !set.has(letter)) {
+            set.add(letter)
+            map.set(word, letter)
+        } else if (map.get(word) !== letter) return false
+    }
+        
+    return true
+};
