bddd7ce9147bbd5b3bb0fa7194f87c0472bb7742
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-06-22 21:47:41 -0400

    Time: 119 ms (20.09%), Space: 44.2 MB (59.84%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1,l2) {
+    
+    let tempHead = new ListNode()
+    let curr = tempHead
+    while(l1 && l2) {
+        if (l1.val < l2.val) {
+            curr.next = l1
+            l1 = l1.next
+        } else {
+            curr.next = l2
+            l2 = l2.next
+        }
+        curr = curr.next
+    }
+    curr.next = l1 || l2
+    return tempHead.next
+};
+
+// Linked List
+// time complexity O(n)
+// space complexity O(1)
