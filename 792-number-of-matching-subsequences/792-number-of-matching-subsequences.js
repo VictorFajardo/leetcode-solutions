4fb5776cf441b2a46d9619cc9c24ad0432f92987
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-07-19 22:46:50 -0400

    Time: 229 ms (61.57%), Space: 48.2 MB (80.99%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
@@ -3,17 +3,26 @@
  * @param {string[]} words
  * @return {number}
  */
-var numMatchingSubseq = function(S, words) {
-    let count = 0
-    words.forEach(word => {
-       let prevIdx = -1
-       for (let i=0; i<word.length; i++) {
-         let letter = word[i]
-         let idx = S.indexOf(letter, prevIdx+1)
-         if (idx > prevIdx) prevIdx = idx
-         else return
-       }
-       count++
-    })
-    return count
+var numMatchingSubseq = function(s, words) {
+    let response = 0;
+    for (let word of words) {
+        if (isWordIncluded(s, word)) response++;
+    }
+    
+    return response;
+};
+
+var isWordIncluded = function(s, word) {
+    let prevIdx = -1;
+    for (let i = 0; i < word.length; i++) {
+        let char = word.charAt(i);
+        let idx = s.indexOf(char, prevIdx + 1);
+        if (idx > prevIdx) {
+            prevIdx = idx;
+        } else {
+            return false
+        }
+    }
+    
+    return true;
 }
