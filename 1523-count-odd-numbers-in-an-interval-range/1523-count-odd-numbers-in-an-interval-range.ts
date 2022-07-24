a0542782bdb2a009d8d31db9b52dddbdaf1515e4
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-07-24 14:16:00 -0400

    Time: 126 ms (46.19%), Space: 43.5 MB (10.17%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.ts
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.ts
@@ -1,8 +1,6 @@
 function countOdds(low: number, high: number): number {
 
-    let response: number = Math.floor((high - low + 1) / 2);
-    
-    response += (low % 2 !== 0 && high % 2 !== 0) ? 1 : 0;
+    let response: number = Math.round((high - low + (low % 2 !== 0 ? 1 : 0)) / 2);
     
     return response;
 };
