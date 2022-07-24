523b424789e535d942427a51733d1a010078887c
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-07-24 16:11:48 -0400

    Time: 183 ms (65.71%), Space: 46.2 MB (91.43%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.ts
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.ts
@@ -2,9 +2,9 @@ function findAnagrams(s: string, p: string): number[] {
     const response: number[] = [];
     const n: number = s.length;
     const m: number = p.length;
-    let size: number = m;
-    let startWindow: number = 0;
-    let endWindow: number = 0;
+    let counter: number = 0;
+    let windowStart: number = 0;
+    let windowEnd: number = 0;
     const frequency: Map<string, number> = new Map();
     
     for (let character of p) {
@@ -12,23 +12,22 @@ function findAnagrams(s: string, p: string): number[] {
         frequency.set(character, frequency.get(character) + 1);
     }
     
-    for (; endWindow < n; endWindow++) {
-        const currentChar: string = s[endWindow];
+    for (; windowEnd < n; windowEnd++) {
+        const currentChar: string = s[windowEnd];
         if (frequency.has(currentChar)) {
-            if (frequency.get(currentChar) > 0) size -= 1;
+            if (frequency.get(currentChar) > 0) counter += 1;
             frequency.set(currentChar, frequency.get(currentChar) - 1);
         }
         
-        while (size === 0) {
-            if (endWindow - startWindow + 1 === m) {
-                response.push(startWindow);
-            }
-            const startChar: string = s[startWindow];
+        if (windowEnd >= m - 1) {
+            if (counter === m) response.push(windowStart);
+            
+            const startChar: string = s[windowStart];
             if (frequency.has(startChar)) {
                 frequency.set(startChar, frequency.get(startChar) + 1);
-                if (frequency.get(startChar) > 0) size += 1;
+                if (frequency.get(startChar) > 0) counter -= 1;
             }
-            startWindow += 1;
+            windowStart += 1;
         }
     }
 
