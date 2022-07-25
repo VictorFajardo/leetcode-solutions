02b8a05c62352c3bbd84cb87202d2d73a784b7fc
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-07-24 22:01:04 -0400

    Time: 125 ms (27.88%), Space: 45.8 MB (10.78%) - LeetHub

--- 67-add-binary/67-add-binary.ts
+++ 67-add-binary/67-add-binary.ts
@@ -1,25 +1,20 @@
 function addBinary(a: string, b: string): string {
-    const arrA: string[] = a.split('');
-    const arrB: string[] = b.split('');
+    let p1: number = a.length - 1;
+    let p2: number = b.length - 1;
     let rest: number = 0;
     let addition: string = '';
     
-    while (arrA.length || arrB.length) {
-        let digitA: number = Number(arrA.pop()) || 0;
-        let digitB: number = Number(arrB.pop()) || 0;
+    while (p1 >= 0 || p2 >= 0 || rest > 0) {
+        rest += p1 >= 0 ? Number(a.charAt(p1)) : 0;
+        rest += p2 >= 0 ? Number(b.charAt(p2)) : 0;
         
-        let sum: number = digitA + digitB + rest;
+        p1 -= 1;
+        p2 -= 1;
         
-        if (sum >= 2) {
-            sum %= 2;
-            rest = 1;
-        } else {
-            rest = 0;
-        }
+        addition = (rest % 2).toString() + addition;
         
-        addition = sum.toString() + addition;
+        rest = Math.floor(rest / 2);
     }
     
-    return rest > 0 ? '1' + addition : addition;
-
+    return addition;
 };
