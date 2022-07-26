f5688a273c346e32d11954d187eb8bc5e8200008
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-07-25 22:48:17 -0400

    Time: 155 ms (60.51%), Space: 53.4 MB (83.78%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.ts
+++ 238-product-of-array-except-self/238-product-of-array-except-self.ts
@@ -1,24 +1,17 @@
 function productExceptSelf(nums: number[]): number[] {
     const n: number = nums.length;
     const response: number[] = [];
-    const leftProducts: number[] = [];
-    const rightProducts: number[] = [];
-    
-    // let left: number = 1;
     
     for (let i = 0; i < n; i++) {
-        leftProducts[i] = i > 0 ? leftProducts[i - 1] * nums[i - 1] : 1;
+        response[i] = i > 0 ? response[i - 1] * nums[i - 1] : 1;
     }    
+    
+    let right: number = 1;
     
     for (let i = n - 1; i >= 0; i--) {
-        rightProducts[i] = i < n - 1 ? rightProducts[i + 1] * nums[i + 1] : 1;
-    }
-    
-    // console.log(leftProducts, rightProducts);
-    
-    for (let i = 0; i < n; i++) {
-        response[i] = leftProducts[i] * rightProducts[i];
-    }    
+        right = i < n - 1 ? right * nums[i + 1]: 1;
+        response[i] *= right;
+    } 
     
     return response;
 };
